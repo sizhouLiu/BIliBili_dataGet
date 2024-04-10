5f5a99ec894da752f61996ed02520f62b08011cb
sizhouLiu <lsz8824523@163.com>
2024-04-10 15:31:48 +0800

    添加__init__文件 打包为PyBilibili

---
+++ PyBiliBili/__init__.py
@@ -0,0 +1,7 @@
+from PyBiliBili.Login import Login
+from PyBiliBili.Bilibili_data_get import Spider,SpidertoDB,VideoSpider
+from PyBiliBili import DefaulString
+
+
+__version__ = '1.0.1'
+__all__ = [Login,Spider,SpidertoDB,VideoSpider]
